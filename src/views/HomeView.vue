dda66f551709ee7fcec4f4d6cd7bf6e9dfdd6b6a
Oakekywe <oakekywephyohann99@gmail.com>
2023-02-04 21:28:31 +0630

    search feature with filter in compostion api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input type="text" v-model="search">
+    <p>search - {{search}}</p>
+    <div v-for="name in filteredName" :key="name">
+      <p>{{name}}</p>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { ref } from '@vue/reactivity'
+import { computed } from '@vue/runtime-core'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+ setup(){
+  let search= ref("")
+  let names= ref(["aung aung", "kyaw kyaw", "Zaw zaw", "khine khine"])
+  let filteredName= computed(()=>{
+    return names.value.filter(name=>{
+      return name.includes(search.value)
+    })
+  })
+
+  return {names, search, filteredName}
+ }
 }
 </script>
