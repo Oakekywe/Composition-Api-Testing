0ce5ea11baa694e11c4b6ea8b064f6842450b588
Oakekywe <oakekywephyohann99@gmail.com>
2023-02-05 21:42:33 +0630

    create page front end

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Detail from '../views/DetailPost.vue'
+import Create from '../views/Create.vue'
 
 const routes = [
   {
@@ -13,6 +14,11 @@ const routes = [
     name: 'Detail',
     component: Detail,
     props: true
+  },
+  {
+    path: '/create',
+    name: 'Create',
+    component: Create
   }
 ]
 
