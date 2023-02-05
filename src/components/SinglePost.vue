da326be5ba3d510c0af152007c0d847927581c32
Oakekywe <oakekywephyohann99@gmail.com>
2023-02-05 11:32:36 +0630

    using props in setup function testing

---
+++ src/components/SinglePost.vue
@@ -0,0 +1,21 @@
+<template>
+    <h2>{{post.title}}</h2>
+    <p>{{cutPostBody}}</p>
+</template>
+
+<script>
+import { computed } from '@vue/runtime-core'
+export default {
+    props:['post'],
+    setup(props){
+        let cutPostBody= computed(()=>{
+            return props.post.body.substring(0,100)+"....";
+        });
+        return {cutPostBody}
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,28 +1,24 @@
 <template>
   <div class="home">
-    <input type="text" v-model="search">
-    <p>search - {{search}}</p>
-    <div v-for="name in filteredName" :key="name">
-      <p>{{name}}</p>
-    </div>
+    <PostsList :posts="posts"></PostsList>
   </div>
+  
 </template>
 
 <script>
-import { ref } from '@vue/reactivity'
-import { computed } from '@vue/runtime-core'
+import PostsList from '../components/PostsList'
+import { ref } from '@vue/reactivity';
+
 
 export default {
+  components: { PostsList },
  setup(){
-  let search= ref("")
-  let names= ref(["aung aung", "kyaw kyaw", "Zaw zaw", "khine khine"])
-  let filteredName= computed(()=>{
-    return names.value.filter(name=>{
-      return name.includes(search.value)
-    })
-  })
-
-  return {names, search, filteredName}
+  let posts =ref([
+    {title: "post title 1",body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque rem repellat fuga, aut deleniti ab impedit hic minus blanditiis, provident possimus mollitia, labore in. Laudantium alias quidem et rem repudiandae?", id:1},
+    {title: "post title 2",body: "Lorem ipsum", id:2},
+    ])
+    return {posts};
+    
  }
 }
 </script>
