33cadd74f69ae8fac52b1331b99500d4e44e9229
Oakekywe <oakekywephyohann99@gmail.com>
2023-02-05 11:53:49 +0630

    life cycle hook testing

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="home">
-    <PostsList :posts="posts"></PostsList>
+    <div v-if="showPosts">
+      <PostsList :posts="posts"></PostsList>
+
+    </div>
+    <button @click="showPosts= !showPosts">Toggle post lists</button>
+    <button @click="posts.pop()">Delete</button>
   </div>
   
 </template>
@@ -13,11 +18,13 @@ import { ref } from '@vue/reactivity';
 export default {
   components: { PostsList },
  setup(){
+  let showPosts= ref(true);
   let posts =ref([
     {title: "post title 1",body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque rem repellat fuga, aut deleniti ab impedit hic minus blanditiis, provident possimus mollitia, labore in. Laudantium alias quidem et rem repudiandae?", id:1},
     {title: "post title 2",body: "Lorem ipsum", id:2},
+    {title: "post title 3",body: "Lorem ipsum", id:3},
     ])
-    return {posts};
+    return {posts, showPosts};
     
  }
 }
